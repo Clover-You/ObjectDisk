01c687cf4060ca7e6f6d511da16493d736ae7b9e
CloverYou <2621869236@qq.com>
2021-11-08 11:10:24 +0800

    👹添加banner👺

--- object-disk/src/main.ts
+++ object-disk/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as path from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(path.join(__dirname, '..', 'resources'), {
+    prefix: '/static/',
+  });
+  await app.listen(3000, () => {
+    showBanner();
+    console.log('👹👹the application start on the 3000 port...🍺🍺');
+  });
+}
+
+/**
+ * 打印banner
+ */
+function showBanner() {
+  try {
+    const bannerPath = path.join(__dirname, '../resources', 'banner.txt');
+    const file = fs.readFileSync(bannerPath);
+    console.log(file.toString());
+  } catch (e) {}
 }
 bootstrap();
