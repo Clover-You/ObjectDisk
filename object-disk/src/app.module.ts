34ddc3efcc4c90e04ba3a674bff949495222f53a
CloverYou <2621869236@qq.com>
2021-11-08 11:38:09 +0800

    配置数据库连接

--- object-disk/src/app.module.ts
+++ object-disk/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
