98fef99edb859b9350e9e40e70355cf4d0ff5134
CloverYou <2621869236@qq.com>
2021-11-09 09:37:48 +0800

    清理无用引用

--- object-disk/src/entity/user_files.entity.ts
+++ object-disk/src/entity/user_files.entity.ts
@@ -1,5 +1,4 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { User } from './user.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 /**
  * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
