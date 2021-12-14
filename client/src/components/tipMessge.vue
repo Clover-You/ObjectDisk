15ce353840f3fa683f58eb11a01185f7ff1ba968
LRolinx <yollx@qq.com>
2021-12-14 22:11:08 +0800

    修改提示为全局提示

--- client/src/components/tipMessge.vue
+++ client/src/components/tipMessge.vue
@@ -1,12 +1,12 @@
 <!--
  * @Author: LRolinx
  * @Date: 2021-01-24 17:44:47
- * @LastEditTime: 2021-01-24 18:22:00
+ * @LastEditTime 2021-12-14 21:42
  * @Description: 提示模态窗
  * 
 -->
 <template>
-  <div class="componentBody" v-if="showTipMessge">
+  <div class="componentBody">
     <div class="contentBox">
       <p>{{title}}</p>
       <span>{{text}}</span>
@@ -15,57 +15,24 @@
 </template>
 <script>
 export default {
-  props: {
-    showTipMessge: {
-      type: Boolean,
-      default: false
-    },
-    title: {
-      type: String,
-      default: "提示"
-    },
-    text: {
-      type: String,
-      default: "这里是测试"
-    }
-  },
+  name: "tipMessge",
   data() {
     return {
-      setTime: null
-    }
+      // tip提示 warn警告 err错误 succ成功
+      type: "tip",
+      title: "提示",
+      text: "你好，世界",
+      time: 2500,
+    };
   },
-  watch: {
-    showTipMessge(newNum) {
-      if (newNum) {
-        if (this.setTime != null) {
-          clearTimeout(this.setTime);
-          this.setTime = null
-        }
-        this.setTime = setTimeout(() => {
-          this.$emit("update:showTipMessge", false);
-        }, 2500);
-      }
-    },
-    title() {
-      if (this.setTime != null) {
-        clearTimeout(this.setTime);
-        this.setTime = null
-      }
-      this.setTime = setTimeout(() => {
-        this.$emit("update:showTipMessge", false);
-      }, 2500);
-    },
-    text() {
-      if (this.setTime != null) {
-        clearTimeout(this.setTime);
-        this.setTime = null
-      }
-      this.setTime = setTimeout(() => {
-        this.$emit("update:showTipMessge", false);
-      }, 2500);
-    }
-  }
-}
+  mounted() {
+    //指定时间后撤销组件
+    setTimeout(() => {
+      this.$destroy(true); //销毁组件
+      this.$el.parentNode.removeChild(this.$el); //父元素中移除dom元素（$el为组件实例）
+    }, this.time);
+  },
+};
 </script>
 <style scoped>
 .contentBox {
